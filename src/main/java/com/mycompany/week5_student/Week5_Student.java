387e40babad1f42f721bcf60cc3fe16e4c8116da
Owner <Owner@10.48.145.180>
2026-02-04 13:40:33 -0500

    create main branch

---
+++ src/main/java/com/mycompany/week5_student/Week5_Student.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.week5_student;
+
+/**
+ *
+ * @author Owner
+ */
+public class Week5_Student {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
